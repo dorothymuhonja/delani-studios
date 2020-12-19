ba9144ce0eb4e7d57fca0f3dbbe5ebb861be6829
dorothymuhonja <dorothymuhonja7@gmail.com>
2020-12-19 23:33:14 +0300

    add hover effect to portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,25 @@
+// what we do
 $(document).ready(function () {
-$('.change-1').click(function(){
-  $('.change-1').slideDown('1500').hide('1000');
-  $('#hide-1').show('1500');
-});
-$('#hide-1').click(function(){
-  $('#hide-1').slideUp('1500');
-  $('.change-1').slideDown('1500');
-});
-
+  $('.change-1').click(function () {
+    $('.change-1').slideDown(1500).hide(1000);
+    $('#hide-1').show(1500);
+  });
+  $('#hide-1').click(function () {
+    $('#hide-1').slideUp(1500);
+    $('.change-1').slideDown(1500);
+  });
 });
 
 $(document).ready(function () {
   $('.change-2').click(function () {
-    $('.change-2').slideDown('1500').hide('1000');
-    $('#hide-2').show('1500');
+    $('.change-2').slideDown(1500).hide(1000);
+    $('#hide-2').show(1500);
   });
   $('#hide-2').click(function () {
-    $('#hide-2').slideUp('1500');
-    $('.change-2').slideDown('1500');
+    $('#hide-2').slideUp(1500);
+    $('.change-2').slideDown(1500);
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 $(document).ready(function () {
   $('.change-3').click(function () {
@@ -51,13 +33,94 @@ $(document).ready(function () {
 });
 
 
+// portfolio
+$(document).ready(function() {
+  $('#port-1').mouseover(function(){
+    $('#grey').show();
+  }).mouseout(function(){
+    $('#grey').hide();
+  });
+});
+
+$(document).ready(function () {
+  $('#port-2')
+    .mouseover(function () {
+      $('#ontario').show();
+    })
+    .mouseout(function () {
+      $('#ontario').hide();
+    });
+});
+
+$(document).ready(function () {
+  $('#port-3')
+    .mouseover(function () {
+      $('#yellow').show();
+    })
+    .mouseout(function () {
+      $('#yellow').hide();
+    });
+});
+
+$(document).ready(function () {
+  $('#port-4')
+    .mouseover(function () {
+      $('#movies').show();
+    })
+    .mouseout(function () {
+      $('#movies').hide();
+    });
+});
+
+$(document).ready(function () {
+  $('#port-5').mouseover(function () {
+      $('#good').show();
+    })
+    .mouseout(function () {
+      $('#good').hide();
+    });
+});
+
+$(document).ready(function () {
+  $('#port-6')
+    .mouseover(function () {
+      $('#button').show();
+    })
+    .mouseout(function () {
+      $('#button').hide();
+    });
+});
+
+$(document).ready(function () {
+  $('#port-7')
+    .mouseover(function () {
+      $('#burned').show();
+    })
+    .mouseout(function () {
+      $('#burned').hide();
+    });
+});
+
+$(document).ready(function () {
+  $('#port-8')
+    .mouseover(function () {
+      $('#restaurant').show();
+    })
+    .mouseout(function () {
+      $('#restaurant').hide();
+    });
+  });
+
+// contact form
+
+
  $(document).ready(function () {
    $('form.formValidation').submit(function () {
       event.preventDefault();
      var name = $('input#name').val();
      var email = $('input#email').val();
      var message = $('textarea#message').val();
-    
+
      if (name && email) {
        alert(name +` your message has been received. We'll get back to you soon.`);
      } else {
